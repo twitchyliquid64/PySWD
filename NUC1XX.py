d3779ddf829b174041189e2ac2bacd6922ba8023
Pascal Hahn <ph@lxd.bz>
2011-12-31 12:12:52 +0100

    writes to data flash working now, app flash still broken

--- NUC1XX.py
+++ NUC1XX.py
@@ -12,52 +12,110 @@ class NotReadyForCommandException(Exception):
   pass
 
 
+class UnlockFailedException(Exception):
+  pass
+
+
 class NUC1XX(object):
-  REGRWPROT_ADDR = 0x50000100
+  CONFIG0_ADDR = 0x00300000
+
   ISPCON_ADDR = 0x5000C000
-  ISPTRG_ADDR = 0x5000C010
   ISPADR_ADDR = 0x5000C004
   ISPDAT_ADDR = 0x5000C008
   ISPCMD_ADDR = 0x5000C00C
+  ISPTRG_ADDR = 0x5000C010
+
+  DHCSR_ADDR = 0xE000EDF0
+
+  DCRSR_ADDR = 0xE000EDF4
+  DCRDR_ADDR = 0xE000EDF8
+
+  AIRCR_ADDR = 0xE000ED0C
+
+  REGRWPROT_ADDR = 0x50000100
 
   def __init__(self, debugport):
     self.ahb = SWDCommon.MEM_AP(debugport, 0)
 
   def halt (self):
-    # halt the processor core
-    self.ahb.writeWord(0xE000EDF0, 0xA05F0003)
+    self.ahb.writeWord(NUC1XX.DHCSR_ADDR, 0xA05F0003)
+
+  def unhalt(self):
+    self.ahb.writeWord(NUC1XX.DHCSR_ADDR, 0xA05F0001)
+
+  def step(self):
+    self.ahb.writeWord(NUC1XX.DHCSR_ADDR, 0xA05F0005)
+
+  def reset(self):
+    self.ahb.writeWord(NUC1XX.AIRCR_ADDR, 0x05FA0004)
 
   def flashUnlock(self):
-    # disable write protection
-    self.ahb.writeBlock(NUC1XX.REGRWPROT_ADDR, [0x59])
-    self.ahb.writeBlock(NUC1XX.REGRWPROT_ADDR, [0x16])
-    self.ahb.writeBlock(NUC1XX.REGRWPROT_ADDR, [0x88])
+    if self.ahb.readBlock(NUC1XX.REGRWPROT_ADDR, 0x01) != [0x00]:
+      print 'flashUnlock: Already unlocked'
+    else:
+      # disable write protection
+      self.ahb.writeWord(NUC1XX.REGRWPROT_ADDR, 0x59)
+      self.ahb.writeWord(NUC1XX.REGRWPROT_ADDR, 0x16)
+      self.ahb.writeWord(NUC1XX.REGRWPROT_ADDR, 0x88)
+    
+    if self.ahb.readBlock(NUC1XX.REGRWPROT_ADDR, 0x01) != [0x01]:
+      raise UnlockFailedException('flachUnlock: Unlock didn\'t work')
 
     # enable ISP
-    self.ahb.writeWord(NUC1XX.ISPCON_ADDR, 0x00000001)
+    self.ahb.writeWord(NUC1XX.ISPCON_ADDR, 0x00000031)
 
   def issueISPCommand(self, adr, cmd, data):
     if self.ahb.readBlock(NUC1XX.ISPTRG_ADDR, 0x01) != [0x00]:
       raise NotReadyForCommandException('not yet ready for command')
     self.ahb.writeWord(NUC1XX.ISPADR_ADDR, adr)
-    self.ahb.writeBlock(NUC1XX.ISPCMD_ADDR, [cmd])
+    self.ahb.writeWord(NUC1XX.ISPCMD_ADDR, cmd)
     self.ahb.writeWord(NUC1XX.ISPDAT_ADDR, data)
 
     # trigger command execution
-    self.ahb.writeBlock(NUC1XX.ISPTRG_ADDR, [0x01])
+    self.ahb.writeWord(NUC1XX.ISPTRG_ADDR, 0x00000001)
+
     while True:
       # successful execution
       if self.ahb.readBlock(NUC1XX.ISPTRG_ADDR, 0x01) == [0x00]:
-        print 'command executed'
         break
       # TODO: for now we assume success, actually verify it
+    ispcon = self.ahb.readWord(NUC1XX.ISPCON_ADDR)
+    self.ahb.writeWord(NUC1XX.ISPCON_ADDR, ispcon)
+    if ispcon & 0x40:
+      print 'issueISPCOMMAND: ISP command failed: %08X' % ispcon
 
   def writeFlash(self):
-    self.issueISPCommand(0x1000, 0x21, 0xDEADBEEF)
+    self.issueISPCommand(0x0001F000, 0x21, 0xDEADBEEF)
 
   def readFlash(self):
-    print str(map(bin, self.ahb.readBlock(0x1000, 0x10)))
+    #self.issueISPCommand(0x0001F000, 0x00, 0x00)
+    print 'read: %s' % hex(self.ahb.readWord(0x0001F000))
 
   def eraseFlash(self):
-    self.issueISPCommand(0x1000, 0x22, 0x00)
+    self.issueISPCommand(0x0001f000, 0x22, 0x00)
 
+  def readRegister(self, register):
+    self.ahb.writeWord(NUC1XX.DCRSR_ADDR, register)
+    return self.ahb.readWord(NUC1XX.DCRDR_ADDR)
+
+  def writeRegister(self, register, data):
+    self.ahb.writeWord(NUC1XX.DCRDR_ADDR, data)
+    self.ahb.writeWord(NUC1XX.DCRSR_ADDR, register)
+
+  def readAllRom(self):
+    for addr in range(0xfffffffC, 0xffffffff, 0x04):
+      print hex(self.ahb.readWord(addr))
+
+  def writeToRam(self):
+    for addr in range(0x20000000, 0x200000d0, 0x04):
+      print 'writing %s' % hex(addr)
+      self.ahb.writeWord(addr, 0xDEADBEEF)
+
+  def readFromRam(self):
+    for addr in range(0x20000000, 0x200000d0, 0x04):
+      print hex(self.ahb.readWord(addr))
+
+  def readConfig(self):
+    self.issueISPCommand(NUC1XX.CONFIG0_ADDR, 0x00, 0x00)
+    print 'readConfig: %s' % hex(
+        self.ahb.readWord(NUC1XX.CONFIG0_ADDR))
---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import serial
+import PirateSWD
+import SWDCommon
+import NUC1XX
+import time
+
+if __name__ == '__main__':
+  pirate = PirateSWD.PirateSWD('/dev/ttyUSB0')
+  debugport = SWDCommon.DebugPort(pirate)
+  nuc1xx = NUC1XX.NUC1XX(debugport)
+  nuc1xx.halt()
+  #nuc1xx.readAllRom()
+  #nuc1xx.readConfig()
+  #nuc1xx.flashUnlock()
+  #nuc1xx.eraseFlash()
+  #nuc1xx.writeFlash()
+  nuc1xx.readFlash()
