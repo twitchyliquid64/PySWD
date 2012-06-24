885334d063a0560e94842c43d7ebf06ee907a01e
Ben Gamari <bgamari.foss@gmail.com>
2012-06-24 14:13:10 -0400

    STM32: Factor out register addresses into constants

--- STM32.py
+++ STM32.py
@@ -1,5 +1,11 @@
 from SWDCommon import *
 
+flash_base = 0x40022004
+
+FLASH_PECR = flash_base+0x04
+FLASH_PEKEYR = flash_base+0x0C
+FLASH_PRGKEYR = flash_base+0x10
+
 class STM32:
     def __init__ (self, debugPort):
         self.ahb = MEM_AP(debugPort, 0)
@@ -17,22 +23,40 @@ class STM32:
         self.ahb.writeWord(0xE000ED0C, 0x05FA0004)
 
     def flashUnlock (self):
-        # unlock main flash
-        self.ahb.writeWord(0x40022004, 0x45670123)
-        self.ahb.writeWord(0x40022004, 0xCDEF89AB)
+        """ unlock data EEPROM """
+        self.ahb.writeWord(FLASH_PEKEYR, 0x89ABCDEF)
+        self.ahb.writeWord(FLASH_PEKEYR, 0x02030405)
 
-    def flashErase (self):
+    def flashLock(self):
+        """ Lock data EEPROM """
+        self.ahb.writeWord(FLASH_PECR, 0x1)
+
+    def programUnlock(self):
+        """ unlock program memory """
+        self.ahb.writeWord(FLASH_PRGKEYR, 0x8C9DAEBF)
+        self.ahb.writeWord(FLASH_PRGKEYR, 0x13141516)
+
+    def programLock(self):
+        """ lock program memory """
+        self.ahb.writeWord(FLASH_PECR, 0x2)
+
+    def flashErase (self, prog, addr):
+        """ erase a page of FLASH """
+        PROG = 1<<3
+        DATA = 1<<4
+        ERASE = 1<<9
         # start the mass erase
-        self.ahb.writeWord(0x40022010, 0x00000204)
-        self.ahb.writeWord(0x40022010, 0x00000244)
+        v = ERASE | (PROG if prog else 0)
+        self.ahb.writeWord(FLASH_PECR, v)
         # check the BSY flag
-        while (self.ahb.readWord(0x4002200C) & 1) == 1:
+        while (self.ahb.readWord(FLASH_PEKEYR) & 1) == 1:
             print "waiting for erase completion..."
             time.sleep(0.01)
-        self.ahb.writeWord(0x40022010, 0x00000200)
+        self.ahb.writeWord(addr, 0x0)
+        self.ahb.writeWord(FLASH_PECR, 0x0)
 
     def flashProgram (self):
-        self.ahb.writeWord(0x40022010, 0x00000201)
+        self.ahb.writeWord(FLASH_PECR, 1<<3)
 
     def flashProgramEnd (self):
-        self.ahb.writeWord(0x40022010, 0x00000200)
+        self.ahb.writeWord(FLASH_PECR, 0x0)
