962768c716830acee9d7de3a03b23fe8c1fe8c47
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-10-20 14:04:45 +0300

    DebugPort: rewrite to use new API and symbolic constants.

--- DebugPort.py
+++ DebugPort.py
@@ -1,3 +1,17 @@
+from SWDProtocol import *
+from SWDErrors import *
+# Debug Port registers
+# p.6-5
+REG_IDCODE = 0 << 3     # Read
+REG_ABORT = 0 << 3      # Write
+REG_CTRL_STAT = 1 << 3  # Read/Write, CTRLSEL=0
+REG_WCR = 1 << 3        # Read/Write, CTRLSEL=1
+REG_RESEND = 2 << 3     # Read
+REG_SELECT = 2 << 3     # Write
+REG_READBUFF = 3 << 3   # Read
+REG_ROUTESEL = 3 << 3   # Write, reserved
+
+
 class DebugPort:
 
     ID_CODES = (
@@ -23,19 +37,19 @@ class DebugPort:
         self.curBank = 0
 
     def idcode(self):
-        return self.swd.readSWD(False, 0)
+        return self.swd.readCmd(OP_DP, REG_IDCODE)
 
-    def abort(self, orunerr, wdataerr, stickyerr, stickycmp, dap):
+    def abort(self, orunerr, wdataerr, stickyerr, stickycmp, dap=False):
         value = 0x00000000
         value = value | (0x10 if orunerr else 0x00)
         value = value | (0x08 if wdataerr else 0x00)
         value = value | (0x04 if stickyerr else 0x00)
         value = value | (0x02 if stickycmp else 0x00)
         value = value | (0x01 if dap else 0x00)
-        self.swd.writeSWD(False, 0, value)
+        self.swd.writeCmd(OP_DP, REG_ABORT, value)
 
     def status(self):
-        return self.swd.readSWD(False, 1)
+        return self.swd.readCmd(OP_DP, REG_CTRL_STAT)
 
     def control(self, trnCount=0, trnMode=0, maskLane=0, orunDetect=0):
         value = 0x54000000
@@ -43,16 +57,16 @@ class DebugPort:
         value = value | ((maskLane & 0x00F) << 8)
         value = value | ((trnMode  & 0x003) << 2)
         value = value | (0x1 if orunDetect else 0x0)
-        self.swd.writeSWD(False, 1, value)
+        self.swd.writeCmd(OP_DP, REG_CTRL_STAT, value)
 
     def select(self, apsel, apbank):
         value = 0x00000000
         value = value | ((apsel  & 0xFF) << 24)
         value = value | ((apbank & 0x0F) << 4)
-        self.swd.writeSWD(False, 2, value)
+        self.swd.writeCmd(OP_DP, REG_SELECT, value)
 
-    def readRB(self):
-        return self.swd.readSWD(False, 3)
+    def readReadbuff(self):
+        return self.swd.readCmd(OP_DP, REG_READBUFF)
 
     def readAP(self, apsel, address):
         adrBank = (address >> 4) & 0xF
@@ -61,7 +75,7 @@ class DebugPort:
             self.select(apsel, adrBank)
             self.curAP = apsel
             self.curBank = adrBank
-        return self.swd.readSWD(True, adrReg)
+        return self.swd.readCmd(OP_AP, adrReg)
 
     def writeAP(self, apsel, address, data, ignore=False):
         adrBank = (address >> 4) & 0xF
@@ -70,4 +84,4 @@ class DebugPort:
             self.select(apsel, adrBank)
             self.curAP = apsel
             self.curBank = adrBank
-        self.swd.writeSWD(True, adrReg, data, ignore)
+        self.swd.writeCmd(OP_AP, adrReg, data, ignore)
